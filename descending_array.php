660257aba2b7187d4b85d5ca57ddd6a04c6eb444
= <=>
2023-08-27 16:50:48 +0530

    reverse array add

--- descending_array.php
+++ descending_array.php
@@ -1,4 +1,6 @@
 <?php
+
+// descending array
  $array = array(1, 5, 2, 10, 4, 6, 15,9);
  $total = count($array);
  
@@ -16,5 +18,17 @@
  }
 
  echo "<pre>";print_r($array);
- 
+echo "<br>";
+
+// reversse array
+$array1 = array(1, 5, 2, 10, 4, 6, 15,9);
+$size = sizeof($array1);
+$reverse = array();
+    for($i=$size-1; $i>=0; $i--)
+    {
+        $reverse[] = $array1[$i];
+    }
+    echo "Before array : 1, 5, 2, 10, 4, 6, 15,9"."<br>";
+    echo "After array :"."<pre>";print_r($reverse);
+    echo "<br>";
 ?>
